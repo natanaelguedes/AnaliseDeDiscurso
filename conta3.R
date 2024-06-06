1a353664f9e1286ef8e360e8b8e5261c9a13471e
Natanael <ngsneto@gmail.com>
2024-06-06 12:13:34 -0300

    add

--- conta3.R
+++ conta3.R
@@ -4,59 +4,181 @@ library(dplyr)
 a=select(data.frame(Ald_aVitPerguntas$Column1.1,Ald_aVitPerguntas$Column1.2))
 Ald_aVitPerguntas
 data.frame(Ald_aVitPerguntas$Column1.1)
-
+modified_data_frame
 table(Ald_aVitPerguntas)
 apply(Ald_aVitPerguntas)
 df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
+df1
 df2= data.frame(table(Ald_aVitPerguntas$Column1.2))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.3))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.4))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.5))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.6))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.7))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.8))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.9))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.10))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.11))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.12))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.13))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.14))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.15))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.16))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.17))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.18))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.19))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
-df1= data.frame(table(Ald_aVitPerguntas$Column1.1))
+df2
+df3= data.frame(table(Ald_aVitPerguntas$Column1.3))
+df3
+df4= data.frame(table(Ald_aVitPerguntas$Column1.4))
+df4
+df5= data.frame(table(Ald_aVitPerguntas$Column1.5))
+df5
+df6= data.frame(table(Ald_aVitPerguntas$Column1.6))
+df6
+df7= data.frame(table(Ald_aVitPerguntas$Column1.7))
+df7
+df8= data.frame(table(Ald_aVitPerguntas$Column1.8))
+df8
+df9= data.frame(table(Ald_aVitPerguntas$Column1.9))
+df9
+df10= data.frame(table(Ald_aVitPerguntas$Column1.10))
+df10
+df11= data.frame(table(Ald_aVitPerguntas$Column1.11))
+df11
+df12= data.frame(table(Ald_aVitPerguntas$Column1.12))
+df12
+df13= data.frame(table(Ald_aVitPerguntas$Column1.13))
+df13
+df14= data.frame(table(Ald_aVitPerguntas$Column1.14))
+df14
+df15= data.frame(table(Ald_aVitPerguntas$Column1.15))
+df15
+df16= data.frame(table(Ald_aVitPerguntas$Column1.16))
+df16
+df17= data.frame(table(Ald_aVitPerguntas$Column1.17))
+df17
+df18= data.frame(table(Ald_aVitPerguntas$Column1.18))
+df18
+df19= data.frame(table(Ald_aVitPerguntas$Column1.19))
+df19
+df20= data.frame(table(Ald_aVitPerguntas$Column1.20))
+df20
+df21= data.frame(table(Ald_aVitPerguntas$Column1.21))
+df21
+df22= data.frame(table(Ald_aVitPerguntas$Column1.22))
+df22
+df23= data.frame(table(Ald_aVitPerguntas$Column1.23))
+df23
+df24= data.frame(table(Ald_aVitPerguntas$Column1.24))
+df24
+df25= data.frame(table(Ald_aVitPerguntas$Column1.25))
+df25
+df26= data.frame(table(Ald_aVitPerguntas$Column1.26))
+df26
+df27= data.frame(table(Ald_aVitPerguntas$Column1.27))
+df27
+df28= data.frame(table(Ald_aVitPerguntas$Column1.28))
+df28
+df29= data.frame(table(Ald_aVitPerguntas$Column1.29))
+df29
+df30= data.frame(table(Ald_aVitPerguntas$Column1.30))
+df30
+df31= data.frame(table(Ald_aVitPerguntas$Column1.31))
+df31
+df32= data.frame(table(Ald_aVitPerguntas$Column1.32))
+df32
+df33= data.frame(table(Ald_aVitPerguntas$Column1.33))
+df33
+df34= data.frame(table(Ald_aVitPerguntas$Column1.34))
+df34
+df35= data.frame(table(Ald_aVitPerguntas$Column1.35))
+df35
+df36= data.frame(table(Ald_aVitPerguntas$Column1.36))
+df36
+df37= data.frame(table(Ald_aVitPerguntas$Column1.37))
+df37
+df38= data.frame(table(Ald_aVitPerguntas$Column1.38))
+df38
+df39= data.frame(table(Ald_aVitPerguntas$Column1.39))
+df39
+df40= data.frame(table(Ald_aVitPerguntas$Column1.40))
+df40
+df41= data.frame(table(Ald_aVitPerguntas$Column1.41))
+df41
+df42= data.frame(table(Ald_aVitPerguntas$Column1.42))
+df42
+df43= data.frame(table(Ald_aVitPerguntas$Column1.43))
+df43
+df44= data.frame(table(Ald_aVitPerguntas$Column1.44))
+df44
+df45= data.frame(table(Ald_aVitPerguntas$Column1.45))
+df45
+df46= data.frame(table(Ald_aVitPerguntas$Column1.46))
+df46
+df47= data.frame(table(Ald_aVitPerguntas$Column1.47))
+df47
+df48= data.frame(table(Ald_aVitPerguntas$Column1.48))
+df48
+df49= data.frame(table(Ald_aVitPerguntas$Column1.49))
+df49
+df50= data.frame(table(Ald_aVitPerguntas$Column1.50))
+df50
+# df50= data.frame(table(Ald_aVitPerguntas$Column1.50))
+# df51= data.frame(table(Ald_aVitPerguntas$Column1.51))
+# df52= data.frame(table(Ald_aVitPerguntas$Column1.52))
+# df53= data.frame(table(Ald_aVitPerguntas$Column1.53))
+# df54= data.frame(table(Ald_aVitPerguntas$Column1.54))
+# df55= data.frame(table(Ald_aVitPerguntas$Column1.55))
+# df57= data.frame(table(Ald_aVitPerguntas$Column1.57))
+# df58= data.frame(table(Ald_aVitPerguntas$Column1.58))
+# df59= data.frame(table(Ald_aVitPerguntas$Column1.59))
+# df60= data.frame(table(Ald_aVitPerguntas$Column1.60))
+# df61= data.frame(table(Ald_aVitPerguntas$Column1.61))
+# df62= data.frame(table(Ald_aVitPerguntas$Column1.62))
+# df63= data.frame(table(Ald_aVitPerguntas$Column1.63))
+# df64= data.frame(table(Ald_aVitPerguntas$Column1.64))
+# df65= data.frame(table(Ald_aVitPerguntas$Column1.65))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.66))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.67))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.68))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.69))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.70))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.71))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.72))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.73))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.74))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.75))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.76))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.77))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.78))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.79))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.80))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.81))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.82))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.83))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.84))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.85))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.86))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.87))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.88))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.89))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.90))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.91))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.92))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.93))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.94))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.95))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.96))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.97))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.98))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.99))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.100))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.101))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.102))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.103))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.103))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.104))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.105))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.106))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.107))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.108))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.109))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.110))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.111))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.112))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.113))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.114))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.115))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.116))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.117))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.118))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.119))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.120))
+# df1= data.frame(table(Ald_aVitPerguntas$Column1.121))
 
 apply(df, 2, function(x) { x[ is.na(x) ] <- ''; df})
 #essa linha abaixo retira os nas do dataframe Aldeia_Vitória_Perguntas
